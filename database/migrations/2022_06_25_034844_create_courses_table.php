4f42ce13c16902bda2ad058d7c95e129e85a0f6a
Marthana <marthanadiva@gmail.com>
2022-06-25 11:03:21 +0700

    add migration

--- database/migrations/2022_06_25_034844_create_courses_table.php
+++ database/migrations/2022_06_25_034844_create_courses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCoursesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->BigIncrements('id');
-            $table->string('name');
-            $table->string('alamat');
-            $table->BigInteger('no_hp');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['user', 'teacher', 'admin']);
+            $table->string('course_title');
+            $table->string('course_image')->nullable();
+            $table->string('subject');
+            $table->integer('course_video')->unsigned()->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('courses');
     }
 }
--- resources/views/template/left-sidebar.blade.php
+++ resources/views/template/left-sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="main-sidebar">
         <aside id="sidebar-wrapper">
           <div class="sidebar-brand">
-          <img src="{{asset('stisla-master/assets/img/logokf.png')}}" width="150px" >
+          <img src="{{asset('assets/img/logokf.png')}}" width="150px" >
           </div>
           <div class="sidebar-brand sidebar-brand-sm">
-          <img src="{{asset('stisla-master/assets/img/logokf2.png')}}" width="30px">
+          <img src="{{asset('assets/img/logokf2.png')}}" width="30px">
           </div>
           <ul class="sidebar-menu">
               <li class="menu-header">Dashboard</li>
@@ -12,7 +12,6 @@
               <li class="menu-header">Starter</li>
           </li>
           <!-- Left side menu ADMIN -->
-          @if (auth()->user()->level == "admin")
           <li class="nav-item">
             <a href="/users" class="nav-link">
             <i class="fas fa-user"></i>
@@ -54,56 +53,7 @@
                 </span>
             </a>
           </li>
-          @endif
-          <!-- Left side menu SELLER -->
-          @if (auth()->user()->level == "seller")
-          <li class="nav-item">
-            <a href="/dashboard" class="nav-link">
-            <i class="fas fa-home"></i>
-                <span>
-                Store
-                </span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/order-seller" class="nav-link">
-            <i class="fas fa-columns"></i> 
-                <span>
-                Order
-                </span>
-            </a>
-          </li>
-          @endif
-          <!-- Left side menu AGENT -->
-          @if (auth()->user()->level == "agent")
-          <li class="nav-item">
-            <a href="/order-agent" class="nav-link">
-            <i class="fas fa-columns"></i> 
-                <span>
-                Order
-                </span>
-            </a>
-          </li>
-          @endif
-          <!-- Left side menu CUSTOMER -->
-          @if (auth()->user()->level == "customer")
-          <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-columns"></i> <span>Order</span></a>
-            <ul class="dropdown-menu">
-              <li><a class="nav-link" href="/order-food">Order Food</a></li>
-              <li><a class="nav-link" href="/order-cust">Order Process</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a href="/payment_customer" class="nav-link">
-            <i class="fas fa-dollar-sign"></i> 
-                <span>
-                Payment
-                </span>
-            </a>
-          </li>
-          @endif
-            
+        
           </li>
             
 
